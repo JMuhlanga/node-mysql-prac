02388525ca40b76dc8a715a65cb8048e298e2352
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2023-08-04 23:33:15 +0300

    First Commit

---
+++ demo_db_connection.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+/* 
+
+// The query method is the one below
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+  });
+});
+
+*/
